85c5f86545b934e5703d301732e23d2cf6ad3337
rodolfo.lacerda <rodolfo.lacerda@memt.com.br>
2019-12-13 18:51:12 -0300

    teste

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,73 +1,24 @@
 const chromedriver = require('chromedriver');
 
-
-const testUrl = 'https://globoesporte.globo.com/'
-const defaultTimeout = 15000
-
 module.exports = {
-    src_folders: ['tests'],
-
-    page_objects_path: './pages',
-
-    webdriver: {
-        start_process: true,
-    },
-
-    // test_workers: {
-    //     enabled: true,
-    //     workers: 2
-    // },
-
-    screenshots: {
-        enabled: true,
-         on_failure: true,
-        on_error: true,
-         path: 'tests_output/'
-    },
-
+    src_folders: ["steps"], //tests is a folder in workspace which has the step definitions
+    page_objects_path: 'pages/', //page_objects folder where selectors are saved
     test_settings: {
         default: {
-            launch_url: testUrl,
-            globals: {
-                waitForConditionTimeout: defaultTimeout // As vezes a conexão com a internet/rede fica lenta
+            screenshots: {
+                enabled: true,
+                path: 'screenshots'
             },
+            launch_url: 'https://globoesporte.globo.com/',
             webdriver: {
+                start_process: true,
                 server_path: chromedriver.path,
-                port: 9515
+                port: 4444,
+                cli_args: ['--port=4444']
             },
             desiredCapabilities: {
-                browserName: "chrome"
+                browserName: 'chrome'
             }
-        },
-
-        headless: {
-            launch_url: testUrl,
-            globals: {
-                waitForConditionTimeout: defaultTimeout // As vezes a conexão com a internet/rede fica lenta
-            },
-            webdriver: {
-                server_path: chromedriver.path,
-                port: 9515
-            },
-            desiredCapabilities: {
-                browserName: "chrome",
-                chromeOptions: {
-                    w3c: false,
-                    args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage']
-                }
-            }
-        },
-
-        stage: {
-            launch_url: "https://globoesporte.globo.com/"
         }
     }
-
-
-}
-
-
-
-
-
-
+};
